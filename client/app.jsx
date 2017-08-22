a34cc3a4560b82639ebef9ac5c1afe5d098f7b88
Abirami Gopinath <abigopi584@gmail.com>
2017-08-22 10:58:50 -0400

    Update and rename app_time_elapsed.jsx to app.jsx

---
+++ client/app.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TimeElapsedAuto from './components/TimeElapsed/TimeElapsedAuto.jsx';
+import TimeElapsedManual from './components/TimeElapsed/Time_Elapsed_manual.jsx'
+
+
+ReactDOM.render(
+    <TimeElapsedAuto  name="Bala" />,
+    document.getElementById('app')
+);
